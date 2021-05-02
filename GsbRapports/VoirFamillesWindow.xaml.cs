26cb98545ccc9dcf75bb99c3fd5dcdf10a952f05
Louis Mbelek <louis.mbelek@gmail.com>
2021-05-02 14:28:08 +0200

    menu visiteur

---
+++ GsbRapports/VoirFamillesWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Net;
+using dllRapportVisites;
+using System.Dynamic;
+using Newtonsoft.Json;
+
+namespace GsbRapports
+{
+    /// <summary>
+    /// Logique d'interaction pour VoirFamillesWindow.xaml
+    /// </summary>
+    public partial class VoirFamillesWindow : Window
+    {
+        private WebClient wb;
+        private Secretaire laSecretaire;
+        private string site;
+        public VoirFamillesWindow(WebClient wb, Secretaire s, string site)
+        {
+            InitializeComponent();
+            this.laSecretaire = s;
+            this.wb = wb;
+            this.site = site;
+            string url = this.site + "familles?ticket=" + this.laSecretaire.getHashTicketMdp();
+            string reponse = this.wb.DownloadString(url);
+            dynamic d = JsonConvert.DeserializeObject(reponse);
+            string familles = d.familles.ToString();
+            string ticket = d.ticket;
+            List<Famille> f = JsonConvert.DeserializeObject<List<Famille>>(familles);
+            this.laSecretaire.ticket = ticket; //important
+            this.dtGrid.ItemsSource = f;
+        }
+
+    }
+
+}
